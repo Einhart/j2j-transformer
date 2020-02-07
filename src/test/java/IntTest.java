6b89367b707f554ff94e4ffa918035e2161867ca
pete knight <pete@knightech.net>
2020-02-07 21:49:13 +0000

    added root node back to json from jsonnide

--- src/test/java/IntTest.java
+++ src/test/java/IntTest.java
@@ -1,6 +1,10 @@
 import com.bazaarvoice.jolt.Chainr;
 import com.bazaarvoice.jolt.JsonUtils;
 import com.bazaarvoice.jolt.utils.JoltUtils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jdk.internal.jline.internal.TestAccessible;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,11 +16,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 import static com.bazaarvoice.jolt.JsonUtils.jsonToObject;
 import static com.bazaarvoice.jolt.utils.JoltUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class IntTest {
@@ -43,19 +49,30 @@ public class IntTest {
     private Chainr transformSpecification;
 
     @Test
-    public void asdf(){
+    public void asdf() throws IOException {
 
-       // String goat = "{\"Photos\":[{\"Id\":\"327703\",\"Caption\":\"TEST>>photo1\",\"Url\":\"http://bob.com/0001/327703/photo.jpg\"},{\"Id\":\"327704\",\"Caption\":\"TEST>>photo2\",\"Url\":\"http://bob.com/0001/327704/photo.jpg\"}]}";
+        /* String goat = "{\"Photos\":[{\"Id\":\"327703\",\"Caption\":\"TEST>>photo1\",\"Url\":\"http://bob.com/0001/327703/photo.jpg\"},{\"Id\":\"327704\",\"Caption\":\"TEST>>photo2\",\"Url\":\"http://bob.com/0001/327704/photo.jpg\"}]}";
+        Object toObject = jsonToObject(goat);*/
 
+        // JsonNode renders the Json without the root
+        String json = "[{\"Id\":\"327703\",\"Caption\":\"TEST>>photo1\",\"Url\":\"http://bob.com/0001/327703/photo.jpg\"},{\"Id\":\"327704\",\"Caption\":\"TEST>>photo2\",\"Url\":\"http://bob.com/0001/327704/photo.jpg\"}]";
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        String goat = "[{\"Id\":\"327703\",\"Caption\":\"TEST>>photo1\",\"Url\":\"http://bob.com/0001/327703/photo.jpg\"},{\"Id\":\"327704\",\"Caption\":\"TEST>>photo2\",\"Url\":\"http://bob.com/0001/327704/photo.jpg\"}]";
-        Object dsfg = jsonToObject(goat);
+        // add the root back on
+        ArrayNode arrayNode = (ArrayNode)objectMapper.readTree(json);
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        JsonNode main = objectNode.set("Photos", arrayNode);
 
-        Object transform = transformSpecification.transform(dsfg);
+        // prepare the object for transformation
+        Object toObject = jsonToObject(main.toString());
+
+        // and transformation...
+        Object transform = transformSpecification.transform(toObject);
 
         final String outbound = JsonUtils.toJsonString(transform);
 
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"+outbound);
+        assertThat(outbound).isNotNull();
+        System.out.println("+ + + : " + outbound + " + + +");
 
     }
 }

8ecfad73af204983ba54d2c852f7eff84eb9428a
pete knight <pete@knightech.net>
2020-02-07 19:31:16 +0000

    updated to latest version of gradle and springboot/batch

--- src/main/java/net/knightech/json/jolt/transformer/Application.java
+++ src/main/java/net/knightech/json/jolt/transformer/Application.java
@@ -3,9 +3,11 @@ package net.knightech.json.jolt.transformer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableConfigurationProperties(LookupProperties.class)
 public class Application {
 
